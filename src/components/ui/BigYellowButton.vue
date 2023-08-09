8cf61beed84c479948b976ed852d17ac81552c61
lintmycode <nuno@nitida.pt>
2023-08-09 13:19:37 +0100

    button styles

--- src/components/ui/BigYellowButton.vue
+++ src/components/ui/BigYellowButton.vue
@@ -19,5 +19,37 @@ button {
   border: 0;
   margin-top: 8rem;
   cursor: pointer;
+  z-index: 1;
+  transition: 100ms all ease-in;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background-color: var(--color-secondary);
+    border-radius: .5rem;
+    z-index: -1;
+  }
+
+  &:before {
+    background-color: var(--color-primary);
+    width: 100%;
+  }
+
+  &:after {
+    width: 0;
+    transition: 200ms all ease-in;
+  }
+
+  &:hover {
+    color: var(--color-background);
+
+    &:after {
+      width: 100%;
+    }
+  }
 }
 </style>
