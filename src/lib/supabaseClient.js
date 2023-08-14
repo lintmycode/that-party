bdf3783e92652e43139375582014186208f508f5
lintmycode <nuno@nitida.pt>
2023-08-14 12:22:34 +0100

    added supabase key to env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://qejbawumcdlpzxnvgxrl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlamJhd3VtY2RscHp4bnZneHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAyMTIwNzgsImV4cCI6MjAwNTc4ODA3OH0.dKyA0gFMgiKlIkXTT0VsKYDgKSjjldqhxgC6F52Fq9o')
+console.log(import.meta.env.VITE_SUPABASE_URL)
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
