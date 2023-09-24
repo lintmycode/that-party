01caf1bf20a5bda7f0d9951d5468ec258105243a
lintmycode <nuno@nitida.pt>
2023-09-24 23:17:54 +0100

    improved photo optimizer

--- scripts/photo-optimizer.js
+++ scripts/photo-optimizer.js
@@ -4,26 +4,47 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const uploadsDir = process.env.UPLOADS_DIR
+const uploadsDir = process.env.UPLOADS_DIR;
+const BATCH_SIZE = 5;
 
 fs.readdir(uploadsDir, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
 
-  files.forEach(file => {
-    const filePath = path.join(uploadsDir, file);
-    const outputPath = path.join(uploadsDir, '../photos', file); // Saving optimized images in a sub-folder
+    // Filter and get only image files
+    const imageFiles = files.filter(file => ['.jpg', '.jpeg', '.png'].includes(path.extname(file).toLowerCase()));
 
-    sharp(filePath)
-      .toFormat('jpeg', { quality: 80 })
-      .toFile(outputPath, (err, info) => {
-        if (err) {
-          console.error('Error processing file:', file, err);
-        } else {
-          console.log('Optimized:', file);
+    // Function to process a batch of images
+    const processBatch = (batchFiles) => {
+        const promises = batchFiles.map(file => {
+            const filePath = path.join(uploadsDir, file);
+            const outputPath = path.join(uploadsDir, '../photos', file);
+
+            return sharp(filePath)
+                .toFormat('jpeg', { quality: 80 })
+                .toFile(outputPath)
+                .then(() => {
+                    console.log('Optimized:', file);
+                })
+                .catch(err => {
+                    console.error('Error processing file:', file, err);
+                });
+        });
+
+        return Promise.all(promises);
+    };
+
+    // Function to process all batches sequentially
+    const processAllBatches = async () => {
+        for (let i = 0; i < imageFiles.length; i += BATCH_SIZE) {
+            const batchFiles = imageFiles.slice(i, i + BATCH_SIZE);
+            await processBatch(batchFiles);
         }
-      });
-  });
+    };
+
+    processAllBatches().then(() => {
+        console.log('Finished processing all images.');
+    });
 });
